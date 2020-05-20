5a9f0f99e73ba95bb65f662fc8e60e842f33ae2f
Hitesh Jain <hiteshjain@Hiteshs-MacBook-Air.local>
2020-05-21 00:44:23 +0530

    Added boilerplate for connecting React fromtend to backend apis

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="" target="_blank">
+            <a className="navbar-brand" href="" target="_blank">
               <img src={logo} width="30" height="30" alt="EduFund" />
             </a>
             <Link to="/" className="navbar-brand">EduFund Survey App!!!!!!</Link>
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Signup extends Component {
     render() {
         return (
             <div>
-                <p>Signup!!</p>
+                <p onClick={this.signupNewUser}>Signup!!</p>
             </div>
         )
+    }
+
+    signupNewUser() {
+        const userObj = {
+            user_name : 'Hitesh Jain',
+            user_email : 'jainhitesh90@gmail.com',
+            user_age : '29',
+        }
+        axios.post('http://localhost:8080/user/add', userObj)
+        .then(res => console.log(res.data));
     }
 }
