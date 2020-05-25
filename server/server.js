cfb9a2a5fcb923926127fed17feacf0fca2d0b44
Hitesh Jain <hiteshjain@Hiteshs-MacBook-Air.local>
2020-05-25 11:54:59 +0530

    Changed mongoDb connection from local database  to remote

--- server/server.js
+++ server/server.js
@@ -1,26 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const app = express();
-require('dotenv').config();
-
 const userRouter = require('./routes/user-routes.js')
 const surveyRouter = require('./routes/survey-routes.js')
+
+const app = express();
 const port = parseInt(process.env.PORT)
+const uri = process.env.DB_URI
+
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully!");
+})
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/user', userRouter);
 app.use('/survey', surveyRouter);
-
-mongoose.connect('mongodb://127.0.0.1:27017/surveys', { useNewUrlParser: true });
-const connection = mongoose.connection;
-
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-})
-
 app.listen(port, function() {
     console.log("Server is running on Port: " + port);
 });
